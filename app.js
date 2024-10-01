95235ffda7ca669af64d3f9ebd09986076a3b5f1
Deepak Somashekhara <dsoma@yahooinc.com>
2024-09-30 18:02:45 -0700

    Adding bootstrap css lib

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/assets/vendor/bootstrap', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')));
 
 app.use('/', indexRouter);
 app.use('/notes', notesRouter);
