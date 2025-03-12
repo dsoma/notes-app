6c7b1da4746e361a8b2670e3b8938a8dd1317a79
Deepak Somashekhara <dsoma@yahooinc.com>
2025-03-12 13:41:33 -0700

    Docker stack deployment

--- terraform/scripts/ec2-login.sh
+++ terraform/scripts/ec2-login.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 aws ecr get-login-password --profile notes-app --region us-west-2 \
-| nerdctl login --username AWS \
+| docker login --username AWS \
 --password-stdin 813234997819.dkr.ecr.us-west-2.amazonaws.com
+# aws ecr get-login-password --profile notes-app --region us-west-2 \
+# | nerdctl login --username AWS \
+# --password-stdin 813234997819.dkr.ecr.us-west-2.amazonaws.com
--- terraform/scripts/infra-setup-steps.sh
+++ terraform/scripts/infra-setup-steps.sh
@@ -1,15 +1,17 @@
-
+# Create EC2 instances for the stack using terraform
 terraform plan
 terraform apply
 
+# Copy key pair file to the public instance
 scp ../../aws/notes-app.pem ubuntu@<PUBLIC_IP>
 ssh ubuntu@<PUBLIC_IP>
 
-# Inside the notes-app-ec2-public instance
+# Inside the notes-app-ec2-public instance: Update the label
 sudo hostname notes-app-public
 sudo docker node update --label-add type=public notes-app-public
 sudo docker node ls
 
+# Join the other private instance/node to the master node/public instance
 sudo docker swarm join-token manager
 ssh -i notes-app.pem <PRIVATE_DNS_NODE1>
 #Execute docker swarm join of node-1 inside node-1
@@ -22,16 +24,18 @@ ssh -i notes-app.pem <PRIVATE_DNS_NODE2>
 docker node update --label-add type=<TYPE> <INSTANCE_NAME>
 exit
 
+# Create a docker context to run docker commands from local
 sudo docker node ls
+docker context use default
 docker context rm ec2
 docker context create ec2 --docker host=ssh://ubuntu@<PUBLIC_IP>
 docker context use ec2
 docker node ls
 
+# create docker secrets
 printf '<TWITTER_CONSUMER_KEY>' | docker secret create TWITTER_CONSUMER_KEY -
 printf '<TWITTER_CONSUMER_SECRET>' | docker secret create TWITTER_CONSUMER_SECRET -
 
 sh ec2-login.sh
 
-
-
+# Adjust Twitter callback url env var and on https://developer.x.com/en/portal/projects/1846644753611517952/apps/29477198/settings
